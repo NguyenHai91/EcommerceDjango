3a0d1ccb5d00da26f6f5fdb4fd04379acb12f333
NguyenHai91 <hainguyen4391@gmail.com>
2022-08-26 11:20:35 +0700

    update media_url in settings

--- Shop/settings.py
+++ Shop/settings.py
@@ -184,7 +184,7 @@ if not DEBUG:
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, ''),)
 
-MEDIA_URL = '/media/'
+MEDIA_URL = ''
 # for template
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # for api reactjs
