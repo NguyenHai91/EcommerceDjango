4ccf591822fd3d10ad7cfda399e9d5c25ce755da
NguyenHai91 <hainguyen4391@gmail.com>
2022-08-30 09:43:10 +0700

    update settings

--- Shop/settings.py
+++ Shop/settings.py
@@ -185,7 +185,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
   STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, ''),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, ''),)
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
