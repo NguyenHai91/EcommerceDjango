12fc016ff3e693c9bd4903911d64a755f54dc723
NguyenHai91 <hainguyen4391@gmail.com>
2022-08-31 10:00:23 +0700

    update setting

--- Shop/settings.py
+++ Shop/settings.py
@@ -194,7 +194,7 @@ MEDIA_URL = '/media/'
 # MEDIA_ROOT = 'media'
 # for api reactjs
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
