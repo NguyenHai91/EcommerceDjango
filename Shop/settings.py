2b7ddad6bd0a72c8780bc694d61cff3e08708080
NguyenHai91 <hainguyen4391@gmail.com>
2022-09-03 21:24:18 +0700

    update static_root

--- Shop/settings.py
+++ Shop/settings.py
@@ -181,7 +181,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_URL = '/static/'
 if not DEBUG:
-  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+  STATIC_ROOT = os.path.join(BASE_DIR, '')
 
 
 
